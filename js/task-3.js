e23997afa8f7e88fcbcd031dd5b320c75941ec4f
Vladyslav Kaplun <tembioz@proton.me>
2025-07-16 18:40:42 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  
+  changeUsername(newUserName) {
+    this.username = newUserName;
+  },
+  
+  updatePlayTime(newTime) {
+    this.playTime += newTime;
+  }
+};
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
